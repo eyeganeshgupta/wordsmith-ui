a35e2949dc46872b6db982b1a901b9ce991e9b0c
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-02-19 20:26:31 +0530

    configured react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <h1 className="px-4 text-2xl font-bold underline">wordsmith</h1>;
+  const HomePage = () => {
+    return (
+      <h1 className="px-4 text-2xl font-bold underline">
+        Welcome to Wordsmith
+      </h1>
+    );
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

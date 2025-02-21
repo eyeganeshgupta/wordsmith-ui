5a845ed58183b240d72b4c7fe3e7e2f5b94c1a91
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-02-21 09:25:16 +0530

    login user action slice and reducer

--- src/redux/slices/users/usersSlice.js
+++ src/redux/slices/users/usersSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const API_BASE_URL = "http://localhost:8087/api/v1";
@@ -27,7 +27,33 @@ export const loginAction = createAsyncThunk(
       const response = await axios.post(`${USERS_API}/login`, credentials);
       return response.data;
     } catch (error) {
-      return rejectWithValue(error?.response?.data || error.message);
+      return rejectWithValue(error?.response?.data || error?.message);
     }
   }
 );
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: INITIAL_STATE,
+  extraReducers: (builder) => {
+    builder.addCase(loginAction.pending, (state) => {
+      state.loading = true;
+    });
+
+    builder.addCase(loginAction.fulfilled, (state, action) => {
+      state.userAuth.userInfo = action.payload;
+      state.loading = false;
+      state.error = null;
+    });
+
+    builder.addCase(loginAction.rejected, (state, action) => {
+      state.error = action.payload;
+      state.userAuth.userInfo = null;
+      state.loading = false;
+    });
+  },
+});
+
+const usersReducer = usersSlice.reducer;
+
+export default usersReducer;

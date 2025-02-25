af8d669896533c9e071b844f284f90ef50452f4d
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-02-25 09:37:49 +0530

    conditionally display navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
+import PrivateNavbar from "./components/Navbar/PrivateNavbar";
 import PublicNavbar from "./components/Navbar/PublicNavbar";
 import Login from "./components/Users/Login";
 import UserProfile from "./components/Users/UserProfile";
 
 function App() {
+  const { userAuth } = useSelector((state) => state?.users);
+  const isLogin = userAuth?.userInfo?.token;
+
   return (
     <BrowserRouter>
-      <PublicNavbar />
+      {isLogin ? <PrivateNavbar /> : <PublicNavbar />}
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />

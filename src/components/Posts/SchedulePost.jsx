546530fa5a3537d302aecb83ea9588c2eb907e94
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-04-23 09:00:50 +0530

    dispatch action to schedule post

--- src/components/Posts/SchedulePost.jsx
+++ src/components/Posts/SchedulePost.jsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
 import { FiCalendar } from "react-icons/fi";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { shedulePostAction } from "../../redux/slices/posts/postsSlice";
+import Error from "../Alert/Error";
+import Loading from "../Alert/Loading";
+import Success from "../Alert/Success";
 
 const SchedulePost = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
-
+  const dispatch = useDispatch();
+  const { postId } = useParams();
+  const { loading, error, success } = useSelector((state) => state?.posts);
   const handleDateChange = (event) => {
     setSelectedDate(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`Selected Date: ${selectedDate}`);
+    dispatch(
+      shedulePostAction({
+        scheduledPublish: selectedDate,
+        postId,
+      })
+    );
   };
 
   return (
     <div className="flex items-center justify-center h-screen bg-green-800">
       <div className="bg-white rounded-lg shadow-md p-6 w-96">
         <h1 className="text-2xl font-bold mb-4 text-green-500">
-          Date Picker Form
+          Schedule Post
         </h1>
         <p className="mb-6 text-gray-600">
           Please select a date from the date picker below
         </p>
+        {error && <Error message={error?.message} />}
+        {success && <Success message="Post has been scheduled successfully" />}
         <form onSubmit={handleSubmit}>
           <label htmlFor="date" className="flex items-center mb-4">
             <FiCalendar className="mr-2 text-blue-500" />
@@ -33,15 +48,16 @@ const SchedulePost = () => {
               className="border-2 border-blue-500 p-2 rounded-md w-full"
             />
           </label>
-          <button
-            type="submit"
-            className="w-full py-2 px-4 bg-green-500 text-white rounded-md"
-          >
-            Submit
-          </button>
-          <button className="flex items-center px-6 py-1 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-700 transition-colors duration-300 ease-in-out">
-            <FiCalendar className="mr-2" /> Schedule Post
-          </button>
+          {loading ? (
+            <Loading />
+          ) : (
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-green-500 text-white rounded-md"
+            >
+              Schedule
+            </button>
+          )}
         </form>
       </div>
     </div>
